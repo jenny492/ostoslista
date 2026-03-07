d6ae01bbaddb0f2b08633e9ece50f9692fc8fe1f
Jenny <205511984+jenny492@users.noreply.github.com>
2026-03-07 14:48:12 +0200

    Tehty tietokantatoiminnot tietokannan luomiselle ja tietojen lisäämiselle. Lisätty tekstikenttä määrälle.

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
+import * as SQLite from 'expo-sqlite';
 import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const db = SQLite.openDatabaseSync('shoppinglistdb');
 
 export default function App() {
 
+  const [amount, setAmount] = useState('');
+  const [product, setProduct] = useState('');
   const [items, setItems] = useState([]);
-  const [item, setItem] = useState('')
 
-  const addItem = () => {
-    setItems([...items, item]);
-    setItem('');
+  const initialize = async () => {
+    try {
+      await db.execAsync(`
+        DROP TABLE IF EXISTS shoppinglist;
+        CREATE TABLE IF NOT EXISTS shoppinglist (id INTEGER PRIMARY KEY NOT NULL, product TEXT, amount TEXT);
+        `);
+      await updateList();
+      const info = await db.getAllAsync("PRAGMA table_info(shoppinglist)");
+      console.log(info);
+    } catch (error) {
+      console.error('Could not open database', error);
+    }
+  }
+  useEffect(() => { initialize() }, []);
+
+  const saveItem = async () => {
+    if (product.length === 0) {
+      return;
+    }
+    try {
+      await db.runAsync('INSERT INTO shoppinglist (product, amount) VALUES (?, ?)', product, amount);
+      await updateList();
+    } catch (error) {
+      console.error('Could not save to database', error)
+    }
   };
 
-  const clearItems = () => {
-    setItems([]);
+  const updateList = async () => {
+    try {
+      const list = await db.getAllAsync('SELECT * from shoppinglist');
+      setItems(list);
+    } catch (error) {
+      console.error('Could not update list', error)
+    }
+  }
+
+  const deleteItem = async (id) => {
+    try {
+      await db.runAsync('DELETE FROM shoppinglist WHERE id=?', id);
+      await updateList();
+    }
+    catch (error) {
+      console.error('Could not delete item', error);
+    }
   }
 
   return (
@@ -21,30 +61,36 @@ export default function App() {
 
       <View style={styles.bodyContainer}>
         <Text style={styles.header}>Shopping list</Text>
-        <Text style={styles.text}>Add an item:</Text>
         <TextInput
+          placeholder='Product'
           style={styles.input}
-          value={item}
-          onChangeText={text => setItem(text)}
+          value={product}
+          onChangeText={product => setProduct(product)}
+        />
+        <TextInput
+          placeholder='Amount'
+          style={styles.input}
+          value={amount}
+          onChangeText={amount => setAmount(amount)}
         />
         <Button
           title="Add"
-          onPress={addItem} />
+          onPress={saveItem} />
       </View>
 
       <View style={styles.bodyContainer}>
         <Text style={styles.header}>Items:</Text>
         <FlatList
           data={items}
+          keyExtractor={item => item.id.toString()}
           renderItem={({ item }) =>
-              <Text style={styles.listItem}>{item}</Text>
+            <View>
+              <Text style={styles.listItem}>{item.product}</Text>
+              <Text>{item.amount}</Text>
+            </View>
           }
         />
-        <Button
-          title="Clear list"
-          onPress={clearItems} />
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -59,10 +105,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     gap: 10,
   },
-  bodyContainer: {
-    marginTop: 20,
-    gap: 10,
-  },
   input: {
     height: 40,
     borderColor: 'gray',
@@ -71,10 +113,6 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  text: {
-    fontSize: 16,
-    marginTop: 20,
   },
   listItem: {
     fontSize: 16,
